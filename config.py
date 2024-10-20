f784e53b46b8df1e8223f8da3b4e1fb014fb4c1a
Johanlieb3 <gosamaye@gmail.com>
2024-10-21 00:14:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # you can find both params here: https://my.telegram.org/apps
-API_ID =21060976 ''
-API_HASH =b8b9e236dd578d07621f5a8174a891d9 ''
+API_ID =21060976 
+API_HASH =b8b9e236dd578d07621f5a8174a891d9 
 
 # @BotFather bot_token
-BOT_TOKEN =8154423087:AAHaLNYJneyIipmVpCOOJpUQvggoNThEcJk ''
+BOT_TOKEN =8154423087:AAHaLNYJneyIipmVpCOOJpUQvggoNThEcJk 
 
 # delay in seconds
 delay = 1
